759355c537780105fd6cd9005ee9d4799805614f
liyw <liyw@funshion.com>
2014-03-07 18:30:08 +0800

    aa

---
+++ Test/src/test/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String s = "a";
+		String ss= "b";
+		System.out.println(s.hashCode());
+		System.out.println(ss.hashCode());
+	}
+
+}
